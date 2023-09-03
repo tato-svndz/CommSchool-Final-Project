d86de18ccfb53d6269fb8ea987ec31e2f4c06df1
Tato Svndz <tato.svndz@gmail.com>
2023-09-03 21:21:50 +0400

    added bunch of hover animations and css + js to make the tabs work in the about section

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Slider 
+let slideIndex = 1; 
 function showSlides(n) {
     let slides = document.getElementsByClassName("slide");
     if (n > slides.length) {
@@ -15,9 +16,28 @@ function showSlides(n) {
 function slideButton(n) {
     showSlides(slideIndex += n);
 }
-function nextSlide() {
-    
-}
-
-let slideIndex = 1; 
 showSlides(slideIndex);
+
+// Tabs
+let tabButtons = document.querySelectorAll('.tab-btn');
+let tabContent = document.querySelectorAll('.tab-content');
+function tabBtn(index) {
+    for (let content of tabContent) {
+        content.style.display = "none";
+        if (content.id == index) {
+            content.style.display = 'flex';
+        } 
+    }
+    for (let btn of tabButtons) {
+        btn.classList.remove('active');
+    }
+    tabButtons[index].classList.add('active');
+    for (let button of tabButtons) {
+        if (!button.classList.contains("active")){
+            button.style.borderBottom = "1px solid #DEE2E6";
+        } else {
+            button.style.borderBottom = 'none';
+        }
+    }
+}
+tabBtn(0); //default
